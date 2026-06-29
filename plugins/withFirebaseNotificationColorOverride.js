7388c3db4985bad69ffc7c6d7d6164cc28712b6a
prateek0128 <rajput.prateek28@gmail.com>
2026-06-29 13:07:57 +0530

    up

--- plugins/withFirebaseNotificationColorOverride.js
+++ plugins/withFirebaseNotificationColorOverride.js
@@ -1,18 +1,21 @@
 const { withAndroidManifest } = require("@expo/config-plugins");
 
-const FCM_NOTIFICATION_COLOR =
-  "com.google.firebase.messaging.default_notification_color";
+const FCM_NOTIFICATION_COLOR = "com.google.firebase.messaging.default_notification_color";
 
 function withFirebaseNotificationColorOverride(config) {
   return withAndroidManifest(config, (config) => {
     const manifest = config.modResults.manifest;
+
+    // Ensure xmlns:tools is declared on the root manifest element
     manifest.$ = manifest.$ || {};
-    manifest.$["xmlns:tools"] = manifest.$["xmlns:tools"] || "http://schemas.android.com/tools";
+    manifest.$["xmlns:tools"] = "http://schemas.android.com/tools";
 
     const application = manifest.application?.[0];
-    const metadata = application?.["meta-data"] || [];
+    if (!application) return config;
 
-    for (const item of metadata) {
+    application["meta-data"] = application["meta-data"] || [];
+
+    for (const item of application["meta-data"]) {
       if (item.$?.["android:name"] === FCM_NOTIFICATION_COLOR) {
         item.$["tools:replace"] = "android:resource";
       }
